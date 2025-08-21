dc8157bd300eb65f5bc000a072155ad08ca41bab
CJxtpro <144556163+CJxtpro@users.noreply.github.com>
2025-08-21 10:53:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,20 +3,21 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23147177"))
-API_HASH = environ.get("API_HASH", "6c1b34bf3c56b9957aab7da8a0dd3482")
+API_ID = int(environ.get("API_ID", "20244134"))
+API_HASH = environ.get("API_HASH", "22d2d4ac9a3cc69038a111721829a80e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "6608709817"))
+OWNER = int(environ.get("OWNER", "8288679033"))
 CREDIT = environ.get("CREDIT", "GOD")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6608709817').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8288679033').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6608709817').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8288679033').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
